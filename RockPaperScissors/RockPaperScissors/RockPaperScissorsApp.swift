a50bb5eb10be1e3393017cb6ed7fa6b3e8b77263
Szymon Tadrzak <szymontadrzak@gmail.com>
2022-03-14 11:39:51 +0000

    RockPaperScissors initial commit

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Szymon Tadrzak on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
